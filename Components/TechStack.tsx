1da8bc402e01842cd504ab06d874634cefa69b53
Inzamam <inzamamchowdhury20@gmail.com>
2024-11-20 10:11:36 +0600

    fix : Tech Stack

--- Components/TechStack.tsx
+++ Components/TechStack.tsx
@@ -5,7 +5,7 @@ const TechStack = () => {
     return (
         <>
 
-            <div className='md:px-10 text-white grid grid-cols-2 md:flex lg:flex justify-center'>
+            <div className='md:px-10 text-white grid grid-cols-2 md:grid-cols-5 lg:grid-cols-5 justify-center'>
                 {TechStacks.map((tech,index) => (
                     <div className=' w-[100px] p-1 flex flex-col justify-center items-center' key={index}>
                         <Image src={tech.imageLink} alt='reactJs' height={50} width={50} unoptimized/>
