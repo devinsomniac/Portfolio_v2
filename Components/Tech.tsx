91fb0a2d9b9a71c13f39a692edf3bae8bfe3b8a7
Inzamam <inzamamchowdhury20@gmail.com>
2024-11-20 21:24:43 +0600

    fix : Tech Stack

--- Components/Tech.tsx
+++ Components/Tech.tsx
@@ -4,7 +4,7 @@ import TechStacks from '@/TechStack'
 
 const Tech = () => {
   return (
-    <div className='text-white grid grid-cols-2 md:grid-cols-6'>
+    <div className='text-white grid grid-cols-4 gap-4'>
         {TechStacks.map((tech,index) => (
            <div key={index} className='flex flex-col justify-center items-center gap-4'>
                 <img src={tech.imageLink} alt="tech" height={45} width={45} />
