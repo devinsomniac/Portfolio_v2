e2d8cdb87a3fefed1156635c1aace376f792b95f
Inzamam <inzamamchowdhury20@gmail.com>
2024-11-20 10:38:29 +0600

    fix : Tech Stack

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,9 @@
-import Header from '@/Components/Header'
 import Signature from '@/Components/Signature'
 import React from 'react'
 
 const layout = ({children}:Readonly<{children : React.ReactNode}>) => {
   return (
     <div>
-        <Header/>
         {children}
         <Signature/>
     </div>
