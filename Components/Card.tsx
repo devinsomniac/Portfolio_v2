7aa2c6cfbfe6f55c62c599395c60ced7baa4004f
devinsomniac <inzamamchowdhury20@gmail.com>
2025-02-13 11:18:28 +0600

    Minor Changes

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React from 'react';
 import Image from 'next/image';
-import Projects from './Projects';
 interface Project {
   id: number;
   name: string;
@@ -13,18 +12,28 @@ interface Project {
 const Card = ({ project } : {project : Project}) => {
   return (
     <a href={project.link} target="_blank" rel="noopener noreferrer">
-    <div className="md:w-[350px] md:h-[300px] shadow-white shadow-sm  bg-slate-50 rounded-xl border text-black flex flex-col justify-center items-center">
+    <div className="
+          md:w-[350px]
+          md:h-[300px] 
+          lg:w-[290px] 
+          lg:[290px] 
+          shadow-white 
+          shadow-sm  
+          bg-slate-50 
+          rounded-xl 
+          border 
+          text-black flex flex-col justify-center items-center">
       <h2 className='font-semibold text-2xl'>{project.name}</h2>
       <div className="p-1">
         <Image
           src={project.image}
           alt="Project Image"
           height={300}
-          width={300}
+          width={250}
           className="border-black border-b-4 rounded-xl shadow-2xl hover:scale-105 transition duration-300 ease-in-out"
         />
       </div>
-      <div className="md:mt-3 flex -space-x-2 overflow-hidden p-3 shadow-2xl rounded-full">
+      <div className="md:mt-3 flex overflow-hidden p-3 shadow-2xl rounded-full">
         {project.tech.map((item, index) => (
           <Image
             src={item}
